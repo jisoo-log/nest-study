b14cbac2ae2f7c7d7e7dc5892c680fd522e0b0a8
jisoo-log <jisoolee.dev@gmail.com>
2022-03-24 18:39:03 +0900

    Add movies controller

---
+++ src/movies/movies.controller.ts
@@ -0,0 +1,46 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+
+@Controller('movies')
+export class MoviesController {
+  @Get()
+  getAll() {
+    return 'This will return all movies';
+  }
+
+  @Get('search')
+  search(@Query('year') year: number) {
+    return `We are searching movie made after: ${year}`;
+  }
+
+  @Get(':id')
+  getOne(@Param('id') movieId: string) {
+    return `This will return one movie with the id: ${movieId}`;
+  }
+
+  @Post()
+  create(@Body() movieData) {
+    return movieData;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') movieId: string) {
+    return `This will delete a movie with the id: ${movieId}`;
+  }
+
+  @Patch(':id')
+  patch(@Param('id') movieId: string, @Body() updateData) {
+    return {
+      updatedMovie: movieId,
+      ...updateData,
+    };
+  }
+}
