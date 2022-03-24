851445872990dba336de6e9b8fdc03ca8df9b769
jisoo-log <jisoolee.dev@gmail.com>
2022-03-24 19:32:34 +0900

    Add UpdateMovieDto

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { MoviesController } from './movies/movies.controller';
-import { MoviesService } from './movies/movies.service';
+import { MoviesModule } from './movies/movies.module';
+import { AppController } from './app.controller';
 
 @Module({
-  imports: [],
-  controllers: [MoviesController],
-  providers: [MoviesService],
+  imports: [MoviesModule],
+  controllers: [AppController], // 보통 모듈은 import 에 넣고, 여기에 기본 홈페이지 내용을 넣는다
+  providers: [],
 })
 export class AppModule {}
